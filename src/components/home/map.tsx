c0d8a1302823b753944e347b560b5e9970e09025
Moumouuu <robin.travail007@gmail.com>
2024-05-13 15:35:46 +0200

    feat(homepage): map with marker

--- src/components/home/map.tsx
+++ src/components/home/map.tsx
@@ -1,11 +1,43 @@
+'use client';
+
 import {H1} from "@/components/common/h1";
 import * as React from "react";
 import {Topbotsvg} from "@/components/common/topbotsvg";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import {MapContainer, TileLayer} from 'react-leaflet'
+import L, {LatLngExpression} from 'leaflet';
 
+interface MapProps {
+    marker: LatLngExpression;
+    label: string;
+}
 
 export const Map = () => {
+    const iconSize = 40;
+    const juihernatureAxes: LatLngExpression = [44.43044099313026, 0.12722739711419073];
+    const icon = L.icon({
+        iconUrl: '/assets/images/map-marker.webp',
+        iconSize: [iconSize, iconSize],
+        iconAnchor: [iconSize / 2, iconSize + 9],
+        className: "Juiher Nature"
+    });
+
+
+    const allMarkers: MapProps[] = [
+        {
+            marker: [44.43044099313026, 0.12722739711419073],
+            label: "Juiher Nature"
+        },
+        {
+            marker: [41.044099313026, 0.13722739711419073],
+            label: "Juiher Nature"
+        },
+        {
+            marker: [43.43044099313026, 0.11722739711419073],
+            label: "Juiher Nature"
+        }
+    ]
+
     return (
         <div className={"relative flex flex-col py-24 bg-juiher-ternary-green p-4 "}>
             <div className={"text-center"}>
@@ -15,11 +47,26 @@ export const Map = () => {
                 Retrouvez tous nos chantiers en cours et à venir sur la carte ci-dessous !
             </span>
 
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer className={"h-[600px]"} center={juihernatureAxes} zoom={6} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <Marker icon={icon} position={juihernatureAxes}>
+                    <Popup>
+                        Juiher Nature
+                    </Popup>
+                </Marker>
+
+                {
+                    allMarkers.map((marker, index) => (
+                        <Marker icon={icon} key={index} position={marker.marker}>
+                            <Popup>
+                                {marker.label}
+                            </Popup>
+                        </Marker>
+                    ))
+                }
 
             </MapContainer>
 
