521bd2d173a14089184bcc8199cd1892ba19769a
Moumouuu <robin.travail007@gmail.com>
2024-05-14 15:53:28 +0200

    feat(ui) fix

--- src/components/common/numberIndex.tsx
+++ src/components/common/numberIndex.tsx
@@ -4,7 +4,7 @@ export const NumberIndex = ({index}: {
     index: number
 }) => {
     return (
-        <div>
+        <div className={"hidden md:block"}>
             <div
                 className={"bg-juiher-secondary-green/70 flex items-center justify-center rounded-full w-12 h-12 p-3 mr-3"}>
                 <span className={'text-xl font-semibold'}>{index + 1}</span>
