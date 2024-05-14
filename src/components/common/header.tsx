9f7f3aecda360a00778f20abe3cc972358e9dae0
Moumouuu <robin.travail007@gmail.com>
2024-05-14 15:55:28 +0200

    feat(ui) fix

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -9,7 +9,9 @@ export const Header = () => {
             <header className={"hidden lg:flex w-full p-3 items-center justify-around bg-juiher-ternary-green"}>
                 <div className={'flex items-center '}>
                     <div className={"h-12 w-12 bg-green-500 rounded-full mr-3"}></div>
-                    <h1 className={"font-semibold"}>Juiher nature</h1>
+                    <h1 className={"font-semibold"}>
+                        <Link href={"/"}>Juiher Nature</Link>
+                    </h1>
                 </div>
                 <Navbar/>
                 <Button variant={"juiher"} asChild>
