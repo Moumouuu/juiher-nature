ebb9ad9d37726912f8642d4bc5bd39f08f3a6c55
Moumouuu <robin.travail007@gmail.com>
2024-05-18 22:03:01 +0200

    feat(home page):  use api for data

--- src/components/home/certifications.tsx
+++ src/components/home/certifications.tsx
@@ -12,8 +12,18 @@ import {
 } from "@/components/ui/carousel";
 import { H1 } from "@/components/common/h1";
 import Image from "next/image";
+import useSWR from "swr";
+import { defaultFetcherGet } from "@/lib/utils";
 
 export function Certifications() {
+  const url =
+    process.env.NEXT_PUBLIC_STRAPI_API_URL + "certifications?populate=*";
+  const { data, error, isLoading } = useSWR(url, defaultFetcherGet);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error during loading certifications</div>;
+  if (data?.data.length === 0) return null;
+
   return (
     <div className={"w-full flex flex-col items-center my-20"}>
       <H1 label={"Nos certifications"} />
@@ -24,22 +34,22 @@ export function Certifications() {
         className="w-[65%] lg:w-[80%]"
       >
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+          {data?.data.map((certif: any) => (
+            <CarouselItem key={certif.id} className="md:basis-1/2 lg:basis-1/3">
               <div className="p-1">
                 <Card>
                   <CardContent className="flex flex-col aspect-square items-center justify-center p-6">
-                    <div className={"bg-red-300 rounded-xl p-14 "}>
+                    <div className={"bg-black/10 rounded-xl p-14 "}>
                       <Image
                         className={"rounded-2xl"}
-                        src={"/assets/images/certifs/SGS.png"}
+                        src={certif.attributes.Logo.data.attributes.url}
                         width={300}
                         height={300}
                         alt={""}
                       />
                     </div>
                     <span className="text-xl font-semibold">
-                      Quali Territoires
+                      {certif.attributes.title}
                     </span>
                   </CardContent>
                 </Card>
